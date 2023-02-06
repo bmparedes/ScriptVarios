a5562fea0bd21d217eccbb131fd3d1a39fe002cf
bmparedes <69495392+bmparedes@users.noreply.github.com>
2023-02-06 14:51:37 -0400

    Check Pages
    Chequea la pagina cada 30 segundos que levante y continua con un segundo script.

---
+++ checkpages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Dirección de la página web a verificar
+url="https://sonar.cgtscorp.com/"
+
+# Intervalo de verificación en segundos
+interval=30
+
+# Bucle infinito
+while :
+do
+  # Verificar si la página web está activa
+  status_code=$(curl -s -o /dev/null -w "%{http_code}" $url)
+  if [ $status_code -eq 200 ]; then
+    # Página web activa, continuar con el siguiente script
+    echo "La página web está activa, continuando con el siguiente script."
+    break
+  else
+    # Página web inactiva, esperar el intervalo
+    echo "La página web no está activa, esperando $interval segundos."
+    sleep $interval
+  fi
+done
+
